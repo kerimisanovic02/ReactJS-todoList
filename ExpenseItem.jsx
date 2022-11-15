2925c254e63d1bb67f94c021930a6f07183e628d
Kerim <95529923+kerimisanovic02@users.noreply.github.com>
2022-11-15 11:17:25 +0100

    Add files via upload

---
+++ ExpenseItem.jsx
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  //const clickHandler = () => {}
+const [title,setTitle] = useState(props.title);
+console.log('ExpenseItem evaluaed by React');
+  
+const clickHandler = () => {
+  setTitle('Updated!')
+  console.log(title);
+};
+
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Klik</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
